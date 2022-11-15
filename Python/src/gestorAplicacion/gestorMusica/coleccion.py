e9252d81b2a2b65d7674959ea6507d468fb650cf
milleracero <mchicaa@unal.edu.co>
2022-11-14 19:39:11 -0500

    parte 1

--- Python/src/gestorAplicacion/gestorMusica/coleccion.py
+++ Python/src/gestorAplicacion/gestorMusica/coleccion.py
@@ -1,1 +1,34 @@
+class Coleccion:
+    
+    coleccionesExistentes=[]
 
+    def __init__(self, usuario, listas, cancionesRecomendadas,colaborativas):
+        self._usuario=usuario
+        self._listas=[]
+        self._cancionesRecomendadas=[]
+        self._colaborativas=[]
+        Coleccion.coleccionesExistentes.append(self)
+    
+    def getUsuario(self):
+        return self._usuario
+    
+    def setUsuario(self,usuario):
+        self._usuario=usuario
+
+    def getListas(self):
+        return self._listas
+    
+    def setListas(self,listas):
+        self._listas=listas
+    
+    @classmethod
+    def getColeccionesExistentes(cls):
+        return cls.coleccionesExistentes
+    
+    @classmethod
+    def setColeccionesExistentes(cls,coleccionesExistentes):
+        cls.coleccionesExistentes=coleccionesExistentes
+    
+    def agregarLista(self,lista):
+        self._listas.append(lista)
+        return ""
