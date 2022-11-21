d90de6088741ebe782149889015de3bdd61cd85f
caroAM22 <caalvarezmu@unal.edu.co>
2022-11-21 17:19:27 -0500

    frame

--- Python/src/interfazGrafica/inicio.py
+++ Python/src/interfazGrafica/inicio.py
@@ -25,7 +25,7 @@ class Inicio(tk.Tk):
         # Creación de la barra de menú
         self.__menu = tk.Menu(self)
         menu_inicio =tk.Menu(self.__menu)
-        menu_inicio.add_command(label = "Descripcion", command = lambda: print("Descripción"))
+        menu_inicio.add_command(label = "Descripcion", command = lambda: self.descripcion())
         menu_inicio.add_command(label = "Salir", command = lambda: self.destroy())
         self.__menu.add_cascade(label = "Inicio", menu = menu_inicio)
         self.config(menu = self.__menu)
@@ -35,6 +35,11 @@ class Inicio(tk.Tk):
         self.__frameIzquierda.grid(row = 0, column = 0, padx = (10,10))
         self.__frameDerecha.grid(row = 0, column = 1, padx = (10,10))
 
+    def descripcion(self):
+        self.__frameIzquierda.__descripcion.pack(pady=(10,0))
+        self.geometry("1420x840")
+
+        
 class FrameIzquierda(tk.Frame):
 
     def __init__(self, ventana):
@@ -44,6 +49,7 @@ class FrameIzquierda(tk.Frame):
         self.__p4_1 = tk.Frame(self)
         self.__p4_2 = tk.Frame(self)
 
+
         # Saludo de bienvenida
         saludo = "¡Bienvenido a Spotifree!"
         self.__saludo = tk.Label(self.__p3, text = saludo, font = ("Segoe Print", 20), fg = "#2C34FA")
@@ -51,8 +57,7 @@ class FrameIzquierda(tk.Frame):
 
         # Descripción del sistema
         descripcion = "Spotifree es un gestor de música del que se puede hacer uso ingresando como usuario. \nCada usuario tiene una colección en la que puede administrar sus listas de reproducción, agregando y eliminando canciones."
-        self.__descripcion = tk.Label(self.__p3, text = descripcion, width = 100, justify = "left", font=("Verdana", 8))
-        self.__descripcion.pack()
+        self.__descripcion = tk.Label(self.__p3, text = descripcion, width = 80, justify = "left", font=("Verdana", 8))
         self.__p3.grid(row = 0, column = 0, pady = (10,10))
 
 class FrameDerecha(tk.Frame):
