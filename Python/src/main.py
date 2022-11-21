33581c756490a0f160678591ca5bd6b138dd5fad
caroAM22 <caalvarezmu@unal.edu.co>
2022-11-21 12:43:40 -0500

    prueba

---
+++ Python/src/main.py
@@ -0,0 +1,29 @@
+from gestorAplicacion.gestorMusica.cancion import Cancion
+from gestorAplicacion.gestorMusica.coleccion import Coleccion
+from gestorAplicacion.gestorMusica.lista import Lista
+from gestorAplicacion.gestorMusica.meGusta import MeGusta
+from gestorAplicacion.gestorMusica.genero import Genero
+from gestorAplicacion.gestorPersonas.persona import Persona
+from gestorAplicacion.gestorPersonas.usuario import Usuario
+from gestorAplicacion.gestorPersonas.artista import Artista
+from baseDatos import serializador
+from baseDatos import deserializador
+
+if __name__=="__main__":
+    a1=Artista("Juanes",Genero.ROCK)
+    a2=Artista("BTS",Genero.KPOP)
+    a3=Artista("Ferxxo",Genero.REGGAETON)
+
+    u1=Usuario("Carolina",Genero.KPOP)
+    u2=Usuario("Miller",Genero.REGGAETON)
+    u3=Usuario("Catalina",Genero.NO_ESPECIFICADO)
+    u4=Usuario("Jeronimo",Genero.ROCK)
+    u5=Usuario("Tomas",Genero.SALSA)
+
+    c1=Cancion("Dynamite",a2,Genero.KPOP,300,2020)
+    c2=Cancion("Camisa negra",a1,Genero.ROCK,200,2015)
+    c3=Cancion("Ferxxo 100",a3,Genero.REGGAETON,360,2019)
+
+    print(Cancion.getCancionesDisponibles())
+    print(Artista.getArtistasDisponibles())
+    print(Usuario.getUsuariosExistentes())
