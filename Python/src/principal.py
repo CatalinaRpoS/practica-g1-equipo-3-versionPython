4d9557b32be1fd1ce0b26464aa15a45cfc929da9
JEROLPOA2 <jledesmap@unal.edu.co>
2022-11-22 21:00:18 -0500

    upload principal

---
+++ Python/src/principal.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+
+class Principal(tk.Tk):
+
+    def __init__(self):
+
+        super().__init__()
+        self.title = "Spotifree"
+        self.resizable(0, 0)
+        
+        ancho_total = self.winfo_screenwidth()
+        alto_total = self.winfo_screenheight()
+        
+        # Aplicamos la siguiente formula para calcular donde debería posicionarse
+
+        self.geometry(str(ancho_total )+"x"+str(alto_total)+"+"+"0"+"+"+"0")
+
+        # MENUUUUUUUUUUS :D
+
+        self.__menubar = tk.Menu(self)
+        self.config(menu= self.__menubar)
+
+        menuArchivo = tk.Menu(self.__menubar, tearoff= 0)
+        self.__menubar.add_cascade(label= "Archivo", menu= menuArchivo)
+        menuProceso = tk.Menu(self.__menubar, tearoff= 0)
+        self.__menubar.add_cascade(label= "Procesos y Consultas", menu= menuProceso)
+        menuAyuda = tk.Menu(self.__menubar, tearoff= 0)
+        self.__menubar.add_cascade(label= "Ayuda", menu= menuAyuda)
+
+        # COMANDOOOOOOOOOOOOS OwO
+
+        menuArchivo.add_command(label= "Aplicacion")
+        menuArchivo.add_command(label= "Salir")
+
+        menuProceso.add_command(label = "Crear usuario")
+        menuProceso.add_command(label = "Crear artista")
+        menuProceso.add_command(label = "Mostrar usuario")
+        menuProceso.add_command(label = "Mostrar artista")
+        menuProceso.add_command(label = "Mostrar cancion")
+        menuProceso.add_command(label = "Acceder")
+
+        menuAyuda.add_command(label = "Acerca de")
+        
+
+
+
+        
+
+
+principal = Principal()
+principal.mainloop()
+
+        
