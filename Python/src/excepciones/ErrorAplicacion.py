c77b449a509f1b71867ed173bc78ba5cd760f165
Catalina Restrepo Salgado <66125382+CatalinaRpoS@users.noreply.github.com>
2022-11-28 23:38:28 -0500

    Delete ErrorAplicacion.py

--- Python/src/excepciones/ErrorAplicacion.py
+++
@@ -1,7 +0,0 @@
-class ErrorAplicacion(Exception):
-
-    def __init__(self, error):
-        self.__error = "Manejo de errores de la Aplicación: " + error
-    
-    def mostrarMensaje(self):
-        return self.__error
